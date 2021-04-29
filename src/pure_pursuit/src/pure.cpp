6a37fe7ee910359884bad64e845726dea9e5d3bb
wa3401 <will.anderson3401@gmail.com>
2021-04-29 12:48:28 -0500

    New pure Pursuit

--- src/pure_pursuit/src/pure.cpp
+++ src/pure_pursuit/src/pure.cpp
@@ -10,6 +10,7 @@
 // Subscribe to a topic with this message type
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float32.h>
+#include <tf2_msgs/TFMessage.h>
 
 #include "std_msgs/String.h"
 #include <string>
@@ -19,141 +20,102 @@
 #include <fstream>
 #include <sstream>
 
+#include "pure_pursuit/csv_reader.h"
+#include "pure_pursuit/pure_pursuit.h"
+#include "pure_pursuit/types.h"
+
 using namespace std;
 
 class Pure {
 
-    struct Waypoint {
-        float X;
-        float Y;
-        float euler;
-        float speed;
-    };
 private:
     ros::NodeHandle n;
 
     ros::Publisher drive_pub;
 
-    ros::Subscriber scan_sub;
-
     ros::Subscriber pose_sub;
 
-    std::vector<Waypoint> waypoints;
+    std::vector<f110::WayPoint> waypoints;
 
-    Waypoint bestPoint;
+    f110::WayPoint bestPoint;
     double bestDif;
-    Waypoint currWay;
+    f110::WayPoint currWay;
     bool firstPoint = true;
+    int num_waypoints;
+
+    size_t uniqueMarkerId;
+    size_t lastBestIdx;
+    tf2_ros::TransformListener tfListener;
+    tf2_ros::Buffer tfBuffer;
 
 public:
 
     
 
-    float L = 2.0;
+    float L = 1.0;
     float wheelbase;
     
-    Pure() {
+    Pure() :
+        n(ros::NodeHandle()),
+        pose_sub(n.subscribe("gt_pose", 5, &Pure::pose_callback, this)),
+        drive_pub(n.advertise<ackermann_msgs::AckermannDriveStamped>("/pure_drive", 1)),
+        lastBestIdx(0),
+        tfListener(tfBuffer)
+    {
         n = ros::NodeHandle("~");
 
         std::string drive_topic, scan_topic, pose_topic;
-        n.getParam("AEB_drive_topic", drive_topic);
+        n.getParam("pure_drive_topic", drive_topic);
         n.getParam("scan_topic", scan_topic);
-        n.getParam("pose_topic", pose_topic);
+        pose_topic = "/gt_pose";
         n.getParam("wheelbase", wheelbase);
+        num_waypoints = 700;
 
-        string temp, line;
-        float X, Y, euler, speed;
+        f110::CSVReader reader("/home/williamanderson/catkin_ws/src/pure_pursuit/src/newPoints.csv");
 
-        ifstream waypointStream("points.csv");
-        if(!waypointStream.is_open()){
-            ROS_INFO("File failed to open");
-        }
+        waypoints = reader.getData(num_waypoints);
 
-        while(getline(waypointStream, line)){
-            stringstream ss(line);
-            getline(ss, temp, ',');
-            X = stof(temp);
-            getline(ss, temp, ',');
-            Y = stof(temp);
-            getline(ss, temp, ',');
-            euler = stof(temp);
-            getline(ss, temp, ',');
-            speed = stof(temp);
-            Waypoint p;
-            p.X = X;
-            p.Y = Y;
-            p.euler = euler;
-            p.speed = speed;
-            waypoints.push_back(p);
-        }
-
-        waypointStream.close();
-
-        drive_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>(drive_topic, 10);
-
-        pose_sub = n.subscribe(pose_topic, 1, &Pure::pose_callback, this);
-
-        
+        ros::Duration(1.0).sleep();
     }
 
-    void pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    void pose_callback(const geometry_msgs::PoseStamped::ConstPtr &msg){
         ackermann_msgs::AckermannDriveStamped drive_st_msg;
         ackermann_msgs::AckermannDrive drive_msg;
 
-        bestPoint.X = 0;
-        bestPoint.Y = 0;
-        bestPoint.euler = 0;
-        bestPoint.speed = 0;
-        bestDif = std::numeric_limits<double>::infinity();
-        float dif;
+        bestPoint = f110::WayPoint(msg);
 
-        if(firstPoint){
-            currWay.X = msg->pose.position.x;
-            currWay.Y = msg->pose.position.y;
-            firstPoint = false;
+        const auto transformedWayPoints = transform(waypoints, bestPoint, tfBuffer, tfListener);
+
+        const auto goalWayPointIdx = f110::get_best_track_point_index(transformedWayPoints, L, lastBestIdx);
+
+        geometry_msgs::TransformStamped mapToBaseLink = tfBuffer.lookupTransform("base_link", "map", ros::Time(0));
+
+        geometry_msgs::Pose goalWayPoint;
+        goalWayPoint.position.x = waypoints[goalWayPointIdx].x;
+        goalWayPoint.position.y = waypoints[goalWayPointIdx].y;
+        goalWayPoint.position.z = 0;
+        goalWayPoint.orientation.x = 0;
+        goalWayPoint.orientation.y = 0;
+        goalWayPoint.orientation.z = 0;
+        goalWayPoint.orientation.w = 0;
+        tf2::doTransform(goalWayPoint, goalWayPoint, mapToBaseLink);
+
+        double steeringAngle = 2*(goalWayPoint.position.y)/(L*L);
+
+        drive_st_msg.header.stamp = ros::Time::now();
+        drive_st_msg.header.frame_id = "base_link";
+
+        drive_st_msg.drive.steering_angle = steeringAngle;
+        
+        if(std::abs(steeringAngle)<.1){
+            drive_st_msg.drive.speed = 6;
         }
-
-        if(std::abs(msg->pose.position.x - currWay.X) < 0.5 || std::abs(msg->pose.position.y - currWay.Y) < 0.5){
-            for(Waypoint p : waypoints){
-                dif = L - std::abs(findDistance(p.X, p.Y, msg->pose.position.x, msg->pose.position.y));
-                if(dif < bestDif){
-                   // ROS_INFO("Found a new waypoint: X: %s Y: %s", std::to_string(p.X), std::to_string(p.Y));
-                    bestPoint.X = p.X;
-                    bestPoint.Y = p.Y;
-                    bestPoint.euler = p.euler;
-                    bestPoint.speed = p.speed;
-                }
-            }
-            currWay.X = bestPoint.X;
-            currWay.Y = bestPoint.Y;
-            currWay.euler = bestPoint.euler;
-            currWay.speed = bestPoint.speed;
-        }
-
-        float distX = currWay.X - msg->pose.position.x;
-        float distY = currWay.Y - msg->pose.position.y;
-
-        float carFrameX = (distX - (distY * std::atan(msg->pose.orientation.w))) * std::acos(msg->pose.orientation.w);
-        float carFrameY = (distX - (distY * std::atan(msg->pose.orientation.w))) * std::asin(msg->pose.orientation.w) + std::acos(msg->pose.orientation.w);
-
-        float radius = std::pow(L, 2) / (2 * std::abs(carFrameY));
-        float curve = 1 / radius;
-        float speed_set = 3;
-
-        float steer_angle = std::atan(curve * wheelbase);
-        if(std::abs(steer_angle)<10){
-            speed_set = 1.5;
-        }
-        else if(std::abs(steer_angle)<20){
-            speed_set = 1;
+        else if(std::abs(steeringAngle)<.2){
+            drive_st_msg.drive.speed = 4;
         }
         else{
-            speed_set = 0.5;
+           drive_st_msg.drive.speed = 3;
         }
-
-        drive_msg.steering_angle = steer_angle;
-        drive_msg.speed = speed_set;
-        drive_st_msg.drive = drive_msg;
         drive_pub.publish(drive_st_msg);
     }
 
@@ -167,7 +129,7 @@ public:
 };
 
 int main(int argc, char ** argv) {
-    ros::init(argc, argv, "AEB_drive");
+    ros::init(argc, argv, "pure_pursuit_node");
     Pure rw;
     ros::spin();
     return 0;
